991abfc8ebf7ad51931de82092fe19ce57ee7d71
duynth1001 <duynth1001@example.com>
2022-10-18 20:23:33 +0700

    Add new check point for merge conflicts

--- helloworld.c
+++ helloworld.c
@@ -5,8 +5,9 @@ int main()
     scanf("%d",&n);
     for (int i = 0; i < 10; i++)
     {
-        printf("This is version 2, i'm in this loop of i=%d\n",i);                
+        printf("This is version 0.1, i'm in this loop of i=%d\n",i);                
     }
+    printf("Check point for merge conflic!");
 //This is a new update
 /*This is another new update!!!*/
 /*This is another new update--version 0.1*/
