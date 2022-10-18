be0b87bac67fe00aaf854d0c63ccffadbd961deb
Danny dre <duynth1001@gmail.com>
2022-10-18 15:12:38 +0700

    Add my first C  HelloWorld fro Visual Studio Code

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Hello, i'm in this loop of i=%d\n",i);        
+    }
+    
+}
