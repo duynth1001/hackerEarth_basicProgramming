942e47aa1780fbfa75974f9b53a6ca1557c9d25c
duynth1001 <duynth1001@gmail.com>
2022-10-18 20:20:21 +0700

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -5,8 +5,9 @@ int main()
     scanf("%d",&n);
     for (int i = 0; i < 10; i++)
     {
-        printf("This is version 2, i'm in this loop of i=%d\n",i);                
+        printf("This is version 2, i'm in this loop of i=%d\n",i);           
     }
+    printf("Check point for version 0.1");
 //This is a new update
 /*This is another new update!!!*/
 /*This is another new update--version 0.1*/
