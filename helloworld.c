cc813f71b48e8c3d09448c646487afe5b5421016
duynth1001 <duynth1001@example.com>
2022-10-18 15:47:08 +0700

    Update file with comment section

--- helloworld.c
+++ helloworld.c
@@ -5,7 +5,7 @@ int main()
     scanf("%d",&n);
     for (int i = 0; i < 10; i++)
     {
-        printf("Hello, i'm in this loop of i=%d\n",i);        
+        printf("Hello, i'm in this loop of i=%d\n",i);                
     }
-    
+//This is a new update
 }
